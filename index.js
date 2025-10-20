a60330d6be68ad495c977166e020f6ae08b0c039
Othallia <vianiputriocta6@gmail.com>
2025-10-20 12:31:58 +0700

    set up gate

--- index.js
+++ index.js
@@ -28,3 +28,14 @@ db.connect((err) => {
     }
     console.log('Connected Succesfully.');
 });
+
+app.get('/api/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) { 
+      console.error('Error Executing query:', + err.stack);
+      res.status(500).send('Error fetching users');
+      return;
+    } 
+    res.json(results); 
+  });
+});
