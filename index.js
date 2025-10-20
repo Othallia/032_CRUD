87928dd2d82563b8a759fee4fc7270a58378a409
Othallia <vianiputriocta6@gmail.com>
2025-10-20 12:26:28 +0700

    connect ke mysql

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '@Ookwlan24',
+    database: 'biodata',
+    port: 3307,
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected Succesfully.');
+});
